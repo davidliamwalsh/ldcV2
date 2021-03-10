ce75221ea8fcc81e1cb900becbc90364b3204781
David <admin@Davids-MacBook-Pro.local>
2021-03-10 17:02:30 +0000

    contact form added

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -1,74 +1,33 @@
-import { Component } from 'react'
-import fetch from 'isomorphic-unfetch'
-import FlashMessage from 'react-flash-message'
+import React from 'react'
+import emailjs from 'emailjs-com'
 
-class ContactForm extends Component {
-  constructor (props) {
-    super(props)
-    this.contactForm = this.contactForm.bind(this)
+const ContactForm = () => {
 
-    this.state = {
-      formSubmitted: false,
-      formFailed: false
-    }
-  }
+  function sendEmail(e) {
+    e.preventDefault();
 
-  async contactForm (event) {
-    this.setState({
-      formSubmitted: false,
-      formFailed: false
-    })
-
-    event.preventDefault()
-    const form = event.target
-    const formData = {
-      From: 'coding@davidwalsh.co.uk',
-      To: 'ldcdecoratingcontractors@davidwalsh.co.uk',
-      ReplyTo: form.email.value,
-      HtmlBody: form.body.value,
-      Name: form.name.value
-    }
-
-    const response = await fetch('/contact-me',
-      {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify(formData)
+    emailjs.sendForm(`${process.env.REACT_APP_EMAILJS_SERVICE_ID}`, `${process.env.REACT_APP_EMAILJS_TEMPLATE_ID}`, e.target, `${process.env.REACT_APP_EMAILJS_USERID}`)
+      .then((result) => {
+          console.log(result.text);
+      }, (error) => {
+          console.log(error.text);
       })
-
-    if (response.status === 200) {
-      this.setState({ formSubmitted: true })
-      form.reset()
-    } else {
-      this.setState({ formFailed: true })
-    }
+      e.target.reset()
   }
 
-  render () {
-    return <>
-    { this.state.formSubmitted
-      ? <FlashMessage duration={3000}>
-        {<div className='c-contact-form__message'>Thanks for your message, I'll get back to you soon</div> }
-      </FlashMessage> : ''}
-    { this.state.formFailed
-      ? <FlashMessage duration={3000}>
-        {<div className='c-contact-form__message'>Whoops... something went wrong, please try again</div> }
-      </FlashMessage> : '' }
-      <div className="c-container">
-        <form onSubmit={this.contactForm}>
-          <div className="c-contact-form">
-            <label className="c-contact-form__name--label" htmlFor="message_name">Name</label>
-            <input className="c-contact-form__name--input" placeholder="Your Name" type="text" name="name" />
-            <label className="c-contact-form__email--label" htmlFor="message_email">Email</label>
-            <input className="c-contact-form__email--input" placeholder="Your e-mail address" type="email" name="email"  />
-            <label className="c-contact-form__body--label" htmlFor="message_body">Body</label>
-            <textarea rows="10" className="c-contact-form__body--input" placeholder="Please type your message" name="body"></textarea>
-          </div>
-          <input type="submit" className="c-contact-form__submit" />
-        </form> 
+  return <div className="c-container">
+    <form className="contact-form" onSubmit={sendEmail}>
+      <div className="c-contact-form">
+        <label className="c-contact-form__body--label">Name</label>
+        <input type="text" name="name" className="c-contact-form__name--input" />
+        <label className="c-contact-form__body--label">Email</label>
+        <input type="email" name="email" className="c-contact-form__name--input" />
+        <label className="c-contact-form__body--label">Message</label>
+        <textarea name="message" rows="10" className="c-contact-form__body--input" placeholder="Please type your message" />
       </div>
-    </>
-  }
+      <input type="submit" value="Send" className="c-contact-form__submit" />
+    </form>
+  </div>
 }
 
 export default ContactForm
